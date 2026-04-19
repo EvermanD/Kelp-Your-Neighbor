c34b510caf123bc326de3ad6fd0b9dbe96f76199
Daniel Everman <daniel@everman.info>
2026-04-19 10:55:40 -0700

    connection limit upped to 10, fixed checkbox sticking out of its container in postpitch/postgig

--- index.mjs
+++ index.mjs
@@ -28,7 +28,7 @@ const pool = mysql.createPool({
     user: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: "nm2pf20notjcum1m",
-    connectionLimit: 2,
+    connectionLimit: 10,
     waitForConnections: true
 });
 
