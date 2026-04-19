2f4ecb1d28d05cc5edddc06a3f6edb4dad4b971c
Jacob Banducci <jbanducci321@gmail.com>
2026-04-19 11:20:06 -0700

    made database management changes

--- index.mjs
+++ index.mjs
@@ -24,10 +24,10 @@ app.use(session({
 }));
 
 const pool = mysql.createPool({
-    host: "k2pdcy98kpcsweia.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    host: "jw0ch9vofhcajqg7.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
     user: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
-    database: "nm2pf20notjcum1m",
+    database: "u2d8f0jswasdnehx",
     connectionLimit: 5,
     waitForConnections: true
 });
